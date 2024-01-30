8b4acb9a219587f985058d81ff1518fa5ef77675
nta45 <nathantayele@gmail.com>
2024-01-29 20:38:12 -0500

    add hello image

--- Week 2/scripts/login.js
+++ Week 2/scripts/login.js
@@ -5,6 +5,6 @@ loginInfo += "<p> Your Username is: " + params.get('username') + "<br>";
 loginInfo += "<p> Your Password is: " + params.get('password');
 
 console.log(loginInfo);
-hi = "Hello " + params.get('username') + "!<br>";
+hi = "Hello " + params.get('username') + "!";
 document.getElementById('card-content').innerHTML = loginInfo;
 document.getElementById('card-title').innerHTML = hi;
