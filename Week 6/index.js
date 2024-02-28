8342b9dc6f6b29578ba6168d0bbbf9b86ab83633
nta45 <nathantayele@gmail.com>
2024-02-28 15:11:36 -0500

    tried to do week 6 but fail first time

--- Week 6/index.js
+++ Week 6/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    }
+);
