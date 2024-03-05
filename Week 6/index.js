91e192cc5200545e8aca02e85468ac1d1b3e8f12
nta45 <nathantayele@gmail.com>
2024-03-04 21:13:20 -0500

    week 6 assignment done

--- Week 6/index.js
+++ Week 6/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const nedb = require('nedb-promises');
+
+app.use(express.static('public'));
+
+const db = nedb.create('hits.jsonl'); // database
+
+app.get('/hits', (req, res) => {
+    db.find({}).then(docs => {
+        if (docs.length) {
+            db.updateOne({"hits":docs[0].hits},{$set: {"hits": docs[0].hits + 1}},{upsert:true})
+        } else {
+            db.insertOne({"hits": 1})
+        }
+
+        res.contentType('text/plain').send((docs[0].hits).toString() );
+    });
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+    res.send({message:"Hello, World!"});
+});
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    }
-);
+app.listen(3000, () => console.log("server is running on port 3000"));
+
