46877dd35f79609fd7b7263bb9979b3b176444f6
Nathan <96786143+nta45@users.noreply.github.com>
2025-03-28 01:36:13 -0400

    true

--- 7-security/index.js
+++ 7-security/index.js
@@ -1,13 +1,12 @@
 const express = require('express');       // load express module
 const nedb = require("nedb-promises");    // load nedb module
-const bcrypt = require('bcrypt');         // load bcrypt module
-const crypto = require('crypto');         // load crypto module
+
 const app = express();                    // init app
 const db = nedb.create('users.jsonl');    // init db
 
 app.use(express.static('public'));        // enable static routing to "./public" folder
-app.use(express.json());                  // decode all requests from JSON and encode all responses into JSON
 
+<<<<<<< HEAD
 // route to authenticate user login
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
@@ -44,17 +43,17 @@ app.post('/register', async (req, res) => {
     await db.update({ username }, { $set: { authToken } });
     res.send({ user: { username: user.username, name: user.name, email: user.email }, auth: authToken });
 });
+=======
+
+// automatically decode all requests from JSON and encode all responses into JSON
+app.use(express.json());
+
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
 
 // create route to get all user records (GET /users)
-app.get('/users', async (req, res) => {
-    // Check if authentication token exists
-    const authToken = req.headers.authorization && req.headers.authorization.split(' ')[1];
-    const user = await db.findOne({ authToken });
-    if (!authToken || !user) {
-        return res.send({ error: 'Unauthorized' });
-    }
-
-    // once authenticated, return all user records
+//   use db.find to get the records, then send them
+//   use .catch(error=>res.send({error})) to catch and send errors
+app.get('/users', (req, res) => {
     db.find({}).then((docs) => {
         res.send(docs);
     }).catch((error) => {
@@ -62,6 +61,7 @@ app.get('/users', async (req, res) => {
     });
 });
 
+<<<<<<< HEAD
 // create route to get a user record (GET /users/:username)
 app.get('/users/:username', async (req, res) => {
     // Check if authentication token exists
@@ -80,18 +80,74 @@ app.get('/users/:username', async (req, res) => {
     }).catch((error) => {
         res.send({ error });
     });
+=======
+
+// create route to get user record (GET /users/:username)
+//   use db.findOne to get user record
+//     if record is found, send it
+//     otherwise, send {error:'Username not found.'}
+//   use .catch(error=>res.send({error})) to catch and send other errors
+app.get('/users/:username', (req, res) => {
+    db.findOne({ "username":req.params.username }).then((docs) => { 
+            if (docs){
+                res.send(docs); 
+            } else { 
+                res.send({ error: 'Username not found.' }) 
+            } 
+        }).catch((error) => {
+            res.send({ error });
+        });
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
 });
 
-// create route to update user doc (PATCH /users/:username)
-app.patch('/users/:username', async (req, res) => {
-    // Check if authentication token exists
-    const authToken = req.headers.authorization && req.headers.authorization.split(' ')[1];
-    const user = await db.findOne({ authToken });
-    if (!authToken || !user) {
-        return res.send({ error: 'Unauthorized' });
+
+// create route to register user (POST /users)
+//   ensure all fields (username, password, email, name) are specified; if not, send {error:'Missing fields.'}
+//   use findOne to check if username already exists in db
+//     if username exists, send {error:'Username already exists.'}
+//     otherwise,
+//       use insertOne to add document to database
+//       if all goes well, send returned document
+//   use .catch(error=>res.send({error})) to catch and send other errors
+app.post('/users', (req, res) => {
+    const { username, password, name, email } = req.body;
+    
+    if (!username && !password && !name && !email) {
+        res.send({ error: 'Missing fields.' });
+        return;
     }
+<<<<<<< HEAD
     // once authenticated, update user record
     db.updateOne({username: req.params.username }, { $set: req.body }).then((docs) => {
+=======
+    db.findOne({ username }).then((docs) => {
+            if (docs) { 
+                res.send({ error: 'Username already exists.' });
+            } else {
+                db.insertOne(req.body)
+                    .then((result) => {
+                        res.send(result);
+                    })
+                    .catch((error) => {
+                        res.send({ error });
+                    });
+            }
+        }).catch((error) => {
+            res.send({ error });
+        });
+});
+
+
+
+// create route to update user doc (PATCH /users/:username)
+//   use updateOne to update document in database
+//     updateOne resolves to 0 if no records were updated, or 1 if record was updated
+//     if 0 records were updated, send {error:'Something went wrong.'}
+//     otherwise, send {ok:true}
+//   use .catch(error=>res.send({error})) to catch and send other errors
+app.patch('/users/:username', (req, res) => {
+    db.updateOne({ "username":req.params.username }, { $set: req.body }).then((docs) => {
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
         if (JSON.stringify(docs).length >= 1) {
             res.send({ ok: true });
         } else {
@@ -103,14 +159,12 @@ app.patch('/users/:username', async (req, res) => {
 });
 
 // create route to delete user doc (DELETE /users/:username)
-app.delete('/users/:username', async (req, res) => {
-    // Check if authentication token exists
-    const authToken = req.headers.authorization && req.headers.authorization.split(' ')[1];
-    const user = await db.findOne({ authToken });
-    if (!authToken || !user) {
-        return res.send({ error: 'Unauthorized' });
-    }
-    // once authenticated, delete user record
+//   use deleteOne to update document in database
+//     deleteOne resolves to 0 if no records were deleted, or 1 if record was deleted
+//     if 0 records were deleted, send {error:'Something went wrong.'}
+//     otherwise, send {ok:true}
+//   use .catch(error=>res.send({error})) to catch and send other errors
+app.delete('/users/:username', (req, res) => {
     db.deleteOne({ "username": req.params.username }).then(r => r.json()).then((docs) => {
         if (docs.length > 1) {
             res.send({ ok: true });
--- 7-security/public/script.js
+++ 7-security/public/script.js
@@ -2,18 +2,30 @@
 
 const $ = document.querySelector.bind(document);
 
+
 // login link action
+<<<<<<< HEAD
 $('#loginLink').addEventListener('click', openLoginScreen);
 // register link action
 $('#registerLink').addEventListener('click', openRegisterScreen);
 // logout link action
 $('#logoutLink').addEventListener('click', openLogoutScreen);
+=======
+$('#loginLink').addEventListener('click',openLoginScreen);
+
+// register link action
+$('#registerLink').addEventListener('click',openRegisterScreen);
+
+// logout link action
+$('#logoutLink').addEventListener('click',openLoginScreen);
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
 
 // Sign In button action
 $('#loginBtn').addEventListener('click', () => {
     // check to make sure username/password aren't blank
     if (!$('#loginUsername').value || !$('#loginPassword').value)
         return;
+<<<<<<< HEAD
 
     // POST provided username/password to authenticate user
     fetch('/login', {
@@ -27,12 +39,30 @@ $('#loginBtn').addEventListener('click', () => {
         })
     }).then(r => r.json()).then((doc) => {
         if (!doc.auth) {
+=======
+     
+    //   GET /users/{username}, where {username} is $('#loginUsername').value
+    //     decode response from json to object called doc
+    //     if doc.error, call showError(doc.error)
+    //     otherwise, if doc.password is NOT the same as $('#loginPassword').value,
+    //       call showError('Username and password do not match.')
+    //     otherwise, call openHomeScreen(doc)
+    //   use .catch(err=>showError('ERROR: '+err)}) to show any other errors
+    fetch('/users/'+$('#loginUsername').value).then(r => r.json()).then((doc)=>{
+        if(doc.error){
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
             showError(doc.error);
+        } else if(doc.password !== $('#loginPassword').value){
+            console.log(doc.password);
+            showError('Username and password do not match.');
         } else {
-            localStorage.setItem('authToken', doc.auth);
             openHomeScreen(doc);
         }
+<<<<<<< HEAD
     }).catch(err => showError('ERROR at log: ' + err));
+=======
+    }).catch(err=>showError('ERROR: '+err));
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
 });
 
 // Register button action
@@ -45,13 +75,14 @@ $('#registerBtn').addEventListener('click', () => {
         showError('All fields are required.');
         return;
     }
-    // grab all user info from input fields, and POST it to /register
+    // grab all user info from input fields, and POST it to /users
     var data = {
         username: $('#registerUsername').value,
         password: $('#registerPassword').value,
         name: $('#registerName').value,
         email: $('#registerEmail').value
     };
+<<<<<<< HEAD
 
     // POST /users and get authentication token
     fetch('/register', {
@@ -60,12 +91,31 @@ $('#registerBtn').addEventListener('click', () => {
         body: JSON.stringify(data)
     }).then(r => r.json()).then((doc) => {
         if (!doc.auth) {
+=======
+     
+    //   POST /users
+    //     convert data (defined above) to json, and send via POST to /users
+    //     decode response from json to object called doc
+    //     if doc.error, showError(doc.error)
+    //     otherwise, openHomeScreen(doc)
+    //   use .catch(err=>showError('ERROR: '+err)}) to show any other errors
+    
+    fetch('/users', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( data )
+    }).then(r => r.json()).then((doc)=>{
+        if(doc.error){
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
             showError(doc.error);
         } else {
-            localStorage.setItem('authToken', doc.auth);
             openHomeScreen(doc);
         }
+<<<<<<< HEAD
     }).catch(err => showError('ERROR at reg: ' + err));
+=======
+    }).catch(err=>showError('ERROR: '+err));
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
 });
 
 // Update button action
@@ -80,6 +130,7 @@ $('#updateBtn').addEventListener('click', () => {
         name: $('#updateName').value,
         email: $('#updateEmail').value
     };
+<<<<<<< HEAD
 
     // getting authToken from localStorage to send with PATCH request
     const authToken = localStorage.getItem('authToken');
@@ -92,6 +143,20 @@ $('#updateBtn').addEventListener('click', () => {
     fetch('/users/' + $('#username').innerText, {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json', 'Authorization': 'Token ' + authToken },
+=======
+     
+    //   PATCH /users/{username}, where {username} is $('#username').innerText
+    //     convert data (defined above) to json, and send via PATCH to /users/{username}
+    //     decode response from json to object called doc
+    //     if doc.error, showError(doc.error)
+    //     otherwise, if doc.ok,
+    //       alert("Your name and email have been updated.");
+    //   use .catch(err=>showError('ERROR: '+err)}) to show any other errors
+    
+    fetch('/users/'+$('#username').innerText, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
         body: JSON.stringify(data)
     }).then(r => r.json()).then((doc) => {
         if (doc.error) {
@@ -99,7 +164,11 @@ $('#updateBtn').addEventListener('click', () => {
         } else if (doc.ok) {
             alert("Your name and email have been updated.");
         }
+<<<<<<< HEAD
     }).catch(err => showError('ERROR at patch: ' + err));
+=======
+    }).catch(err=>showError('ERROR: '+err));
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
 });
 
 // Delete button action
@@ -107,6 +176,7 @@ $('#deleteBtn').addEventListener('click', () => {
     // confirm that the user wants to delete
     if (!confirm("Are you sure you want to delete your profile?"))
         return;
+<<<<<<< HEAD
 
     // getting authToken from localStorage to send with PATCH request
     const authToken = localStorage.getItem('authToken');
@@ -150,6 +220,35 @@ function showListOfUsers() {
             showError(docs.error);
             return;
         }
+=======
+     
+    //   DELETE /users/{username}, where {username} is $('#username').innerText
+    //     decode response from json to object called doc
+    //     if doc.error, showError(doc.error)
+    //     otherwise, openLoginScreen()
+    //   use .catch(err=>showError('ERROR: '+err)}) to show any other errors
+    fetch('/users/'+$('#username').innerText,{
+        method: 'DELETE'
+    }).then((doc)=>{
+        if(doc.error){
+            showError(doc.error);
+        } else {
+            console.log("Opened login screen");
+            openLoginScreen();  
+        }
+    }).catch(err=>{console.log("errored");showError('ERROR: '+err);});
+});
+
+function showListOfUsers(){
+    
+    //   GET /users
+    //     decode response from json to an array called docs
+    //     for every doc in docs, call showUserInList(doc)
+    //       you can do this by using a for-loop or, better yet, a forEach function:
+    //         docs.forEach(showUserInList)
+    //   use .catch(err=>showError('Could not get user list: '+err)}) to show any potential errors
+    fetch('/users').then(r => r.json()).then((docs)=>{
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
         docs.forEach(showUserInList);
     }).catch(err => showError('Could not get user list: ' + err));
 }
@@ -182,12 +281,12 @@ function openHomeScreen(doc) {
     showError('');
     // reveal home screen
     $('#homeScreen').classList.remove('hidden');
-    // to display name, username
-    $('#name').innerText = doc.user.name;
-    $('#username').innerText = doc.user.username;
+    // display name, username
+    $('#name').innerText = doc.name;
+    $('#username').innerText = doc.username;
     // display updatable user info in input fields
-    $('#updateName').value = doc.user.name;
-    $('#updateEmail').value = doc.user.email;
+    $('#updateName').value = doc.name;
+    $('#updateEmail').value = doc.email;
     // clear prior userlist
     $('#userlist').innerHTML = '';
     // show new list of users
@@ -204,6 +303,7 @@ function openLoginScreen() {
     $('#loginScreen').classList.remove('hidden');
 }
 
+<<<<<<< HEAD
 function openLogoutScreen() {
     // hide other screens, clear inputs, clear error
     $('#registerScreen').classList.add('hidden');
@@ -219,6 +319,9 @@ function openLogoutScreen() {
 }
 
 function openRegisterScreen() {
+=======
+function openRegisterScreen(){
+>>>>>>> parent of 310c4f5 (finished assignment 7-security)
     // hide other screens, clear inputs, clear error
     $('#loginScreen').classList.add('hidden');
     $('#homeScreen').classList.add('hidden');
